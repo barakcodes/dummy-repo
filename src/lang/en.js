1c2d0211dd220aa9bfe83933dc769fcee483b955
barak_codes <35469348+barakcodes@users.noreply.github.com>
2021-03-10 02:45:22 -0800

    Update en.js

--- src/lang/en.js
+++ src/lang/en.js
@@ -2,6 +2,6 @@ module.exports = {
   en: {
     starwars: "may the force be with you all!",
     yoda:"translate me we will!",
-    yolo:"translate me we will!",
+    yolo:"translate me you will!",
      }
 };
